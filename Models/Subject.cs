59e9ae1b73338c6fef09b7afb6ae0e7d97369df9
Rafael Simões <rafael.simoes.06@gmail.com>
2021-02-07 06:11:36 +0000

    Added EF, BD, Models and starting views

---
+++ Models/Subject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Subject
+    {
+        public int SubjectID { get; set; }
+
+        public Course Course { get; set; }
+
+        public Teacher Teacher { get; set; }
+    }
+}
